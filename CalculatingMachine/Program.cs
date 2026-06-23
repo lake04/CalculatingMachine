ed8bb709e82db99507590fc292096808f415eca7
lake04 <wjdghksrla0409@gmail.com>
2026-06-23 23:43:52 +0900

    Edit | 정수 부분 후위표기식으로 변환

--- CalculatingMachine/Program.cs
+++ CalculatingMachine/Program.cs
@@ -7,42 +7,97 @@ class Program
     {
         string str;
         Stack<double> numStack = new Stack<double>();
-        Stack<char> rearStr = new Stack<char>();
-        Stack<char> operatorStack = new Stack<char>();
+        List<string> rearStr = new List<string>();
+        Stack<string> operatorStack = new Stack<string>();
 
         Console.WriteLine("입력 : ");
 
         str = Console.ReadLine();
 
         int len = str.Length;
+        char preChar = '\n';
+        string nums = string.Empty;
+        string opperator = string.Empty;
 
         for (int i = 0; i < len; i++)
         {
-            if (str[i] >= '0' && str[i] <= '9')
+            if(preChar != '\n')
             {
-                rearStr.Push(str[i]);
-            }
-            else if (str[i] == '*' || str[i] == '/' || str[i] == '+' || str[i] == '-')
-            {
-                operatorStack.Push(str[i]);
-            }
-            else if (str[i] == ')')
-            {
-                char oppratorStack = operatorStack.Pop();
-                rearStr.Push(oppratorStack);
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    nums += str[i];
+                }
+                else if (preChar >= '0' && preChar <= '9' && str[i] == '*' || str[i] == '/' || str[i] == '+' || str[i] == '-' )
+                {
+                    if (nums.Length > 0)
+                    {
+                        rearStr.Add(nums);
+                        nums = string.Empty;
+                    }
+                    opperator += str[i];
+                    operatorStack.Push(opperator);
+                }
+                else if(str[i] == '(')
+                {
+
+                }
+                else if (preChar >= '0' && preChar <= '9' && str[i] == ')')
+                {
+                    if (nums.Length > 0)
+                    {
+                        rearStr.Add(nums);
+                        nums = string.Empty;
+                    }
+                    string oppratorStack = operatorStack.Pop();
+                    rearStr.Add(oppratorStack);
+                }
+                else
+                {
+                    Console.WriteLine("잘못 된 입력입니다.");
+                }
             }
             else
             {
-                Console.WriteLine("잘못 된 입력입니다.");
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    nums += str[i];
+                }
+                else if (str[i] == '*' || str[i] == '/' || str[i] == '+' || str[i] == '-')
+                {
+                    opperator += str[i];
+                    operatorStack.Push(opperator);
+                }
+                else if (str[i] == '(')
+                {
+
+                }
+                else if (str[i] == ')')
+                {
+                    string oppratorStack = operatorStack.Pop();
+                    rearStr.Add(oppratorStack);
+                }
+                else
+                {
+                    Console.WriteLine("잘못 된 입력입니다.");
+                }
             }
+            preChar = str[i];
+            opperator = string.Empty;
         }
+        if(nums.Length > 0)
+        {
+            rearStr.Add(nums);
+        }
+
+
+        int index = 0;
 
         while (rearStr.Count > 0)
         {
-            char cur = rearStr.Pop();
-            if (cur >= '0' && cur <= '9')
+            string cur = rearStr[index++];
+            if (cur[0] >= '0' && cur[0] <= '9')
             {
-                numStack.Push(cur - '0');
+                numStack.Push(cur[0] - '0');
             }
             else
             {
@@ -58,7 +113,7 @@ class Program
                 {
                     num2 = numStack.Pop();
                 }
-                switch (cur)
+                switch (cur[0])
                 {
                     case '*':
                         sum = num1 * num2;
